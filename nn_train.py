a938d699430eeb4b34a604f2eab4c505ffa7dcec
Fabio Dias <fabio.dias@gmail.com>
2018-09-28 16:43:53 -0400

    testing 2 layer (0.951 train)

--- nn_train.py
+++ nn_train.py
@@ -73,6 +73,10 @@ def main(argv):
         test_handle = sess.run(test_iterator.string_handle())
 
         builder = tf.saved_model.builder.SavedModelBuilder(FLAGS.model_dir)
+        builder.add_meta_graph_and_variables(sess,
+                            ["trained"],
+                            strip_default_attrs=True)
+
         for step in range(FLAGS.max_steps):
           _ = sess.run([train_op, accuracy_update_op],
                       feed_dict={handle: train_handle})
@@ -83,25 +87,26 @@ def main(argv):
             print("\nStep: {:>3d} Loss: {:.3f} Accuracy: {:.3f}".format(
                 step, loss_value, accuracy_value))
             xlog.append(step)
-            tlog.append(loss_value)
+            tlog.append(accuracy_value)
             loss_value, accuracy_value = sess.run(
                 [elbo_loss, accuracy], feed_dict={handle: heldout_handle})
             print("Step: {:>3d} Loss: {:.3f} Accuracy: {:.3f}".format(
                 step, loss_value, accuracy_value))
-            vlog.append(loss_value)
+            vlog.append(accuracy_value)
             plt.figure()
             plt.plot(xlog,tlog,'b')
             plt.plot(xlog,vlog,'r')
             plt.legend(['Train','Validation'])
             plt.savefig('curves.png',dpi=300)
             plt.close()
+            builder.save()
           if  ( step == (FLAGS.max_steps-1)):# or ((step % 1000) == 0):
             builder.add_meta_graph_and_variables(sess,
                                        ["trained"],
                                        strip_default_attrs=True)
             builder.save()
-          if  ( step == (FLAGS.max_steps-1)) or ((step % 1000) == 0):            
-            neural_net.save_weights(os.path.join(FLAGS.model_dir,'w{0}.h5'.format(step)))
+          # if  ( step == (FLAGS.max_steps-1)) or ((step % 1000) == 0):            
+          #   neural_net.save_weights(os.path.join(FLAGS.model_dir,'w{0}.h5'.format(step)))
 
 
     loss_value, accuracy_value = sess.run([elbo_loss, accuracy], feed_dict={handle: test_handle})
