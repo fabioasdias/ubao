7b4460f4800cf7971a35706b8519e5ce034da022
Fabio Dias <fabio.dias@gmail.com>
2019-03-15 13:43:40 -0400

    properly working, i hope

--- createVideos.py
+++ createVideos.py
@@ -40,7 +40,7 @@ for i in range(NumH):
 
 
 #10 minutes at 25frames/second 
-numFrames=10*60*25
+numFrames=10*60*25 #10*60
 
 panels=glob('Panel*.txt')
 
@@ -73,38 +73,33 @@ for panel in panels:
             while len(newImages)<cN:
                 minUsed=np.min(uses)
                 possibles=list(np.atleast_1d(np.squeeze(np.where(uses==minUsed))))
-                for ii in possibles:
-                    if uses[ii]!=minUsed:
-                        print('wrong!!!', ii,uses[ii],minUsed)
-                        exit()
 
                 if (len(newImages)+len(possibles))>cN:
-                    possibles=choice(possibles,size=(cN-len(newImages),))
+                    possibles=possibles[:(cN-len(newImages))]
                 for img in possibles:
                     uses[img]+=1
-                newImages.extend(possibles)
-
                 
+                newImages.extend(possibles)
                 
             for img in newImages:
                 ind=choice(list(range(total)),size=(1,),p=_h2p(np.sum(to_use!=-1,axis=0)))
                 to_use[i,ind]=img
 
-
-
-
     print('duplicates')
 
     for j in tqdm(range(total)):
         if (np.max(to_use[:,j]))==-1:
-            to_use[:,j]=randint(total)
+            to_use[:,j]=choice(list(range(N)),size=(1,),p=_h2p(uses))
+            uses[to_use[0,j]]+=1
         else:
-            used=list(np.squeeze(np.where(to_use[:,j]!=-1)))
+            used=list(np.atleast_1d(np.squeeze(np.where(to_use[:,j]!=-1))))
             to_use[used[-1]:,j]=to_use[used[-1],j]
             to_use[:used[0]:,j]=to_use[used[-1],j]
             for i in range(1,len(used)):
                 to_use[used[i-1]:used[i],j]=to_use[used[i],j]
+
     # for i in range(numFrames):
+    #     print(i,np.any(to_use[i,:]>N),to_use[i,:],N)
     #     print(i,np.squeeze(to_use[i]).astype(np.int))
     #     input('.')
             
@@ -119,7 +114,11 @@ for panel in panels:
     for i in tqdm(range(numFrames)):
         for j,img in enumerate(to_use[i].squeeze()):
             if (i==0) or (to_use[i,j]!=to_use[i-1,j]):
+                # try:
                 newIm=np.array(Image.open(join('./img/',imagelist[img])).resize((sW,sH),Image.BILINEAR))
+                # except:
+                #     print(img,len(imagelist))
+                #     exit()
                 if (len(newIm.shape)==2):
                     newIm=np.stack([newIm,newIm,newIm],axis=2)            
                 frame[grid[j][0]:grid[j][0]+sH,grid[j][1]:grid[j][1]+sW,:]=newIm[:,:,0:3]#RGBA
